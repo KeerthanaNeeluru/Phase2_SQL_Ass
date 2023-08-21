3541452ce685885b35f615bce0518192ea722dd1
keerthana <keerthananeeluru2001@gmail.com>
2023-08-21 15:32:36 +0530

    Adding day 7 assignment

---
+++ A7/LibDb.sql
@@ -0,0 +1,14 @@
+create database LibraryDB
+use LibraryDB
+create table Books
+(
+	BookId int primary key,
+	Title nvarchar(50),
+	Author nvarchar(50),
+	Genre nvarchar(50),
+	Quantity int
+)
+
+
+insert into Books values(1,'The Hidden Hindu Triology','Akshat Gupta','Religious & Spiritual Fiction',3),
+						(2,'Moving target','Christina Diaz Gonzalez','Children Stories',2)
