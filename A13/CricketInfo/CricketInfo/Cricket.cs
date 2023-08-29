75389b58047b3b65a20b6a837e574df7c017c2eb
keerthana <keerthananeeluru2001@gmail.com>
2023-08-29 15:55:02 +0530

    Adding Assignment 13

---
+++ A13/CricketInfo/CricketInfo/Cricket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CricketInfo
+{
+    [Table("Cricket")]
+    public class Cricket
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TeamName { get; set; }
+        [Required]
+        public int NOWC { get; set; }
+    }
+}
---
+++ A13/WebAppCricInfo/WebAppCricInfo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppCricInfo.MvcApplication" Language="C#" %>
