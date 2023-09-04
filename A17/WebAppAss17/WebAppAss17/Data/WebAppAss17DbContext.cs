603466508a54b8aef35774308237570c442ebfa4
keerthana <keerthananeeluru2001@gmail.com>
2023-09-04 14:24:13 +0530

    adding day17

---
+++ A17/WebAppAss17/WebAppAss17/Data/WebAppAss17DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppAss17.Models;
+
+namespace WebAppAss17.Data
+{
+    public class WebAppAss17DbContext : DbContext
+    {
+        public WebAppAss17DbContext (DbContextOptions<WebAppAss17DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppAss17.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ A17/WebAppAss17/WebAppAss17/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppAss17.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAppAss17DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppAss17DbContext") ?? throw new InvalidOperationException("Connection string 'WebAppAss17DbContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
